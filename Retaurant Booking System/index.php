18838bd2799c28f687cc0326e37a529c655126ff
tze cheng <imchenchen1994@hotmail.com>
2014-10-20 11:27:10 +0800

    Added login link and signup link in index

--- Retaurant Booking System/index.php
+++ Retaurant Booking System/index.php
@@ -4,6 +4,16 @@
     include("Includes/header.php");
 ?>
     This is the main page
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <div>
+            <div>
+                <p> <a href="/login.php"> Log In here!</a></p>
+                <p> <a href="/signup.php"> Sign up here!</a></p>
+            </div>
+        </div>
+
 <?php
     include ("Includes/footer.php");    
 
