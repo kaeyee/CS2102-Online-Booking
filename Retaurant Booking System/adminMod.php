7a3ee9669e228504314762a4ff00b279fb244f25
yu yao <luiyuyao@hotmail.com>
2014-10-26 17:08:33 +0800

    minor refactorings of adminMod

--- Retaurant Booking System/adminMod.php
+++ Retaurant Booking System/adminMod.php
@@ -23,7 +23,6 @@
 </script>
 
 <script type="text/javascript">
-
     function deleteRow(id) {
         if (confirm("Sure you want to delete this update? There is NO undo!")) {
             $url = "delete.php?ID=";
@@ -36,8 +35,6 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
-
     <head>
         <meta charset="utf-8" />
         <title></title>
@@ -45,8 +42,6 @@
             td {
                 border:  1px solid black;
             }
-
-         
         </style>
     </head>
     <body>
@@ -54,8 +49,7 @@
             <fieldset>
                 <legend>Search for Booking Record based on Email or Location</legend>
                 Email: <input type="text" id="email" name="email"> 
-                <br>
-                <br>
+                <br><br>
                 <select name="location" id="location">
                 <option value="">Select Location</option>
                 <?php
@@ -63,8 +57,7 @@
                 $statement = $databaseConnection ->prepare($query);
                 $statement -> execute();
                 $result = $statement->get_result();
-                while($row = $result ->fetch_array(MYSQLI_NUM)) 
-                {
+                while($row = $result ->fetch_array(MYSQLI_NUM)){
                     foreach($row as $r){
                        echo"<option value=\"".$r."\">".$r."</option><br>";
                     }
@@ -80,62 +73,57 @@
     if(isset($_POST['submit'])){
         $emailSubmitted = trim($_POST['email']);
         $locationSubmitted = $_POST['location'];
+
         if($emailSubmitted == "" && $locationSubmitted == ""){
             echo "Please fill in at least 1 field!";
-        }
-        
-        else{
-        if($emailSubmitted != "" && $locationSubmitted != ""){
-            $query = "SELECT B_Id, Email_Address, Time, Date, No_Table, Location, Remark, Created_On FROM booking_record WHERE Email_Address = ? AND Location = ?";
-            $statement = $databaseConnection -> prepare($query);
-            $statement -> bind_param('ss', $emailSubmitted, $locationSubmitted);
-        }else if($emailSubmitted != "" && $locationSubmitted == ""){
-            $query = "SELECT B_Id, Email_Address, Time, Date, No_Table, Location, Remark, Created_On FROM booking_record WHERE Email_Address = ?";
-            $statement = $databaseConnection -> prepare($query);
-            $statement -> bind_param('s', $emailSubmitted);
-        }else if($emailSubmitted == "" && $locationSubmitted != ""){
-            $query = "SELECT B_Id, Email_Address, Time, Date, No_Table, Location, Remark, Created_On FROM booking_record WHERE Location = ?";
-            $statement = $databaseConnection -> prepare($query);
-            $statement -> bind_param('s', $locationSubmitted);
-        }
+        }else{
+            if($emailSubmitted != "" && $locationSubmitted != ""){
+                $query = "SELECT B_Id, Email_Address, Time, Date, No_Table, Location, Remark, Created_On FROM booking_record WHERE Email_Address = ? AND Location = ?";
+                $statement = $databaseConnection -> prepare($query);
+                $statement -> bind_param('ss', $emailSubmitted, $locationSubmitted);
+            }else if($emailSubmitted != "" && $locationSubmitted == ""){
+                $query = "SELECT B_Id, Email_Address, Time, Date, No_Table, Location, Remark, Created_On FROM booking_record WHERE Email_Address = ?";
+                $statement = $databaseConnection -> prepare($query);
+                $statement -> bind_param('s', $emailSubmitted);
+            }else if($emailSubmitted == "" && $locationSubmitted != ""){
+                $query = "SELECT B_Id, Email_Address, Time, Date, No_Table, Location, Remark, Created_On FROM booking_record WHERE Location = ?";
+                $statement = $databaseConnection -> prepare($query);
+                $statement -> bind_param('s', $locationSubmitted);
+            }
 
-        //$query = "SELECT B_Id, Email_Address, Time, Date, No_Table, Location, Remark, Created_On FROM booking_record WHERE Email_Address = ? AND Location = ?";
-       // $statement = $databaseConnection -> prepare($query);
-        //$statement -> bind_param('ss', $emailSubmitted, $locationSubmitted);
-        $statement -> execute();
-        $statement -> bind_result($B_Id, $Email_Address, $Time, $Date, $No_Table, $Location, $Remark, $Created_On);
+            $statement -> execute();
+            $statement -> bind_result($B_Id, $Email_Address, $Time, $Date, $No_Table, $Location, $Remark, $Created_On);
        
             // create table's titles
             echo '<table align="left" cellspacing="5" cellpadding="8">
             <tr>
-                <td align="left"><b>B_Id</b></td>
-                <td align="left"><b>Email_Address</b></td>
-                <td align="left"><b>Time</b></td>
-                <td align="left"><b>Date</b></td>
-                <td align="left"><b>No_Table</b></td>
-                <td align="left"><b>Location</b></td>
-                <td align="left"><b>Remark</b></td>
-                <td align="left"><b>Created_On</b></td>
-            </tr>'; 
+                    <td align="left"><b>B_Id</b></td>
+                    <td align="left"><b>Email_Address</b></td>
+                    <td align="left"><b>Time</b></td>
+                    <td align="left"><b>Date</b></td>
+                    <td align="left"><b>No_Table</b></td>
+                    <td align="left"><b>Location</b></td>
+                    <td align="left"><b>Remark</b></td>
+                    <td align="left"><b>Created_On</b></td>
+             </tr>'; 
         
-        while($statement -> fetch()){
-            echo'<tr><td align="left">' .
-                $B_Id             . '</td><td align="left">' .
-                $Email_Address    . '</td><td align="left">' .
-                $Time             . '</td><td align="left">' .
-                $Date             . '</td><td align="left">' .
-                $No_Table           . '</td><td align="left">' .
-                $Location         . '</td><td align="left">' .
-                $Remark           . '</td><td align="left">' .
-                $Created_On       . '</td><td align="left">';
-                echo '<input type="submit" name="edit" id= "'.$B_Id.'" value="Edit" onclick= "editRow(id)" />'; 
-                echo '<input type="submit" name="delete" id= "'.$B_Id.'" value="Delete" onclick= "deleteRow(id)"/>';
-               // <td><input id="'.$B_Id.'" class="delete" type="button" value="Delete" /></td>
-                echo '</tr>';
-        }
-        echo '</table>';
-        
-     }
+            while($statement -> fetch()){
+                echo'<tr><td align="left">' .
+                    $B_Id             . '</td><td align="left">' .
+                    $Email_Address    . '</td><td align="left">' .
+                    $Time             . '</td><td align="left">' .
+                    $Date             . '</td><td align="left">' .
+                    $No_Table           . '</td><td align="left">' .
+                    $Location         . '</td><td align="left">' .
+                    $Remark           . '</td><td align="left">' .
+                    $Created_On       . '</td><td align="left">';
+                    echo '<input type="submit" name="edit" id= "'.$B_Id.'" value="Edit" onclick= "editRow(id)" />'; 
+                    echo '<input type="submit" name="delete" id= "'.$B_Id.'" value="Delete" onclick= "deleteRow(id)"/>';
+                   // <td><input id="'.$B_Id.'" class="delete" type="button" value="Delete" /></td>
+                    echo '</tr>';
+            }
+            echo '</table>';
+         }
     }
     ?>
         
