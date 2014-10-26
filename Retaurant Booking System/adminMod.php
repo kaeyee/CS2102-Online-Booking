9b1e217ac0d27a87cbb0a43585b10d18d868ea2b
yu yao <luiyuyao@hotmail.com>
2014-10-26 15:06:18 +0800

    change no_pax to no_table in AdminMod

--- Retaurant Booking System/adminMod.php
+++ Retaurant Booking System/adminMod.php
@@ -77,12 +77,11 @@
     if(isset($_POST['submit'])){
         $emailSubmitted = trim($_POST['email']);
         
-        $query = "SELECT B_Id, Email_Address, Time, Date, No_Pax, Location, Remark, Created_On FROM booking_record WHERE Email_Address = ?";
+        $query = "SELECT B_Id, Email_Address, Time, Date, No_Table, Location, Remark, Created_On FROM booking_record WHERE Email_Address = ?";
         $statement = $databaseConnection -> prepare($query);
         $statement -> bind_param('s', $emailSubmitted);
         $statement -> execute();
-        $statement -> bind_result($B_Id, $Email_Address, $Time, $Date, $No_Pax, $Location, $Remark, $Created_On);
-
+        $statement -> bind_result($B_Id, $Email_Address, $Time, $Date, $No_Table, $Location, $Remark, $Created_On);
        
             // create table's titles
             echo '<table align="left" cellspacing="5" cellpadding="8">
@@ -91,7 +90,7 @@
                 <td align="left"><b>Email_Address</b></td>
                 <td align="left"><b>Time</b></td>
                 <td align="left"><b>Date</b></td>
-                <td align="left"><b>No_Pax</b></td>
+                <td align="left"><b>No_Table</b></td>
                 <td align="left"><b>Location</b></td>
                 <td align="left"><b>Remark</b></td>
                 <td align="left"><b>Created_On</b></td>
@@ -103,7 +102,7 @@
                 $Email_Address    . '</td><td align="left">' .
                 $Time             . '</td><td align="left">' .
                 $Date             . '</td><td align="left">' .
-                $No_Pax           . '</td><td align="left">' .
+                $No_Table           . '</td><td align="left">' .
                 $Location         . '</td><td align="left">' .
                 $Remark           . '</td><td align="left">' .
                 $Created_On       . '</td><td align="left">';
